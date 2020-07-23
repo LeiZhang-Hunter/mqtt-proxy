5b4490082615a06feb0948adb98e54aa60fe991a
zhanglei <zhanglei1@chelun.com>
2020-07-23 12:50:08 +0800

    加入代理中通知上报操作

--- src/MQTTSessionHandle.cc
+++ src/MQTTSessionHandle.cc
@@ -45,14 +45,58 @@ bool DeviceServer::MQTTSessionHandle::OnConnect(const DeviceServer::Callback::MQ
 bool DeviceServer::MQTTSessionHandle::OnSubscribe(const DeviceServer::Callback::MQTTClientSessionPtr&  session,
         const DeviceServer::MQTTSubscribe& subscribe)
 {
-    std::cout<<"Subscribe"<<std::endl;
+    MQTTProxy::MQTTProxyProtocol protocol;
+    //连接的标志
+    protocol.MessageType = MQTTProxy::SUBSCRIBE;
+    //返回码
+    protocol.MessageNo = MQTTProxy::RETURN_OK;
+    //客户端id的长度
+    protocol.ClientIdLength = session->getClientId().length();
+    //客户端id
+    protocol.ClientId = session->getClientId();
+    //查看是否要输入账号密码，如果要输入则需要在载荷中加入账号和密码
+    Json::Value proto_builder;
+    proto_builder["topic"] = subscribe.topic;
+    if(proto_builder.size() > 0)
+    {
+        Json::String encode_string = MQTTContainer.Util.jsonEncode(proto_builder);
+        protocol.Payload.insert(
+                protocol.Payload.end(),
+                encode_string.begin(),
+                encode_string.end());
+    }
+    protocol.MessageLength = protocol.Payload.size();
+    //发送消息到设备中心
+    MQTTContainer.getProxyClient()->sendProxyData(protocol);
 }
 
 //取消订阅事件
 bool DeviceServer::MQTTSessionHandle::OnUnSubscribe(const DeviceServer::Callback::MQTTClientSessionPtr&  session,
         const DeviceServer::MQTTSubscribe& subscribe)
 {
-
+    MQTTProxy::MQTTProxyProtocol protocol;
+    //连接的标志
+    protocol.MessageType = MQTTProxy::UNSUBSCRIBE;
+    //返回码
+    protocol.MessageNo = MQTTProxy::RETURN_OK;
+    //客户端id的长度
+    protocol.ClientIdLength = session->getClientId().length();
+    //客户端id
+    protocol.ClientId = session->getClientId();
+    //查看是否要输入账号密码，如果要输入则需要在载荷中加入账号和密码
+    Json::Value proto_builder;
+    proto_builder["topic"] = subscribe.topic;
+    if(proto_builder.size() > 0)
+    {
+        Json::String encode_string = MQTTContainer.Util.jsonEncode(proto_builder);
+        protocol.Payload.insert(
+                protocol.Payload.end(),
+                encode_string.begin(),
+                encode_string.end());
+    }
+    protocol.MessageLength = protocol.Payload.size();
+    //发送消息到设备中心
+    MQTTContainer.getProxyClient()->sendProxyData(protocol);
 }
 
 //收到相关主题的推送
@@ -60,11 +104,37 @@ void DeviceServer::MQTTSessionHandle::OnPublish(const DeviceServer::Callback::MQ
         const DeviceServer::MQTTSubscribe& subscribe, const std::string& message
 )
 {
-
+    MQTTProxy::MQTTProxyProtocol protocol;
+    //连接的标志
+    protocol.MessageType = MQTTProxy::UNSUBSCRIBE;
+    //返回码
+    protocol.MessageNo = MQTTProxy::RETURN_OK;
+    //客户端id的长度
+    protocol.ClientIdLength = session->getClientId().length();
+    //客户端id
+    protocol.ClientId = session->getClientId();
+    //查看是否要输入账号密码，如果要输入则需要在载荷中加入账号和密码
+    protocol.Payload.insert(
+            protocol.Payload.end(),
+            message.begin(),
+            message.end());
+    protocol.MessageLength = protocol.Payload.size();
+    //发送消息到设备中心
+    MQTTContainer.getProxyClient()->sendProxyData(protocol);
 }
 
 //关闭会话触发的事件
 bool DeviceServer::MQTTSessionHandle::OnDisConnect(const DeviceServer::Callback::MQTTClientSessionPtr&  session)
 {
-
+    MQTTProxy::MQTTProxyProtocol protocol;
+    //连接的标志
+    protocol.MessageType = MQTTProxy::UNSUBSCRIBE;
+    //返回码
+    protocol.MessageNo = MQTTProxy::RETURN_OK;
+    //客户端id的长度
+    protocol.ClientIdLength = session->getClientId().length();
+    //客户端id
+    protocol.ClientId = session->getClientId();
+    //发送消息到设备中心
+    MQTTContainer.getProxyClient()->sendProxyData(protocol);
 }
